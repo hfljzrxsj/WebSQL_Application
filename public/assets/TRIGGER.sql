b69fa6e4b493941d4f4548517a6ce491e58a9425
黄俊学 <2506370693@qq.com>
2024-04-09 11:19:36 +0800

    push

--- public/assets/TRIGGER.sql
+++ public/assets/TRIGGER.sql
@@ -1,4 +1,4 @@
-CREATE TRIGGER IF NOT EXISTS GradeLogTrigger AFTER UPDATE ON Grade FOR EACH ROW 
+CREATE TRIGGER IF NOT EXISTS GradeLogTrigger AFTER UPDATE OF score ON Grade FOR EACH ROW 
 BEGIN
     INSERT INTO GradeLog VALUES (NEW.student_id,NEW.course_id,OLD.score,NEW.score,datetime('now'));
 END;
